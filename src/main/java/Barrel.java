59ffdc0f885719489c0139bb983934f9c27fe67e
Noah Murphy <noamurphy@upei.ca>
2020-10-29 19:29:15 -0300

    Completed Barrel class

--- src/main/java/Barrel.java
+++ src/main/java/Barrel.java
@@ -1,2 +1,12 @@
 public class Barrel {
+    private int amtOfRum = 0;
+    public Barrel(int amtOfRum){
+        this.amtOfRum = amtOfRum;
+    }
+    public void setAmtOfRum(int amtOfRum){
+        this.amtOfRum = amtOfRum;
+    }
+    public int getAmtOfRum(){
+        return amtOfRum;
+    }
 }
